8739f721c6f20316491d3dd451fab5a8fd261404
Kaua281 <Kos281@hotmail.com>
2024-08-06 11:24:44 -0300

    Reestruturacao DrawerNavigation

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { View, Text } from 'react-native';
+
+const Page = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>teste</Text>
+        </View>
+    );
+};
+
+export default Page;
