dcb51a20b1d91487972e4426e8450624a3521b97
Kaua281 <Kos281@hotmail.com>
2024-08-07 11:14:24 -0300

    develop: Criando componente de Search para ser implementado

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import SearchBox from '../components/SearchBox'; 
 
 const Home = () => {
     return (
         <View className="flex-1 items-center justify-center">
             <Text className="bg-orange-500 font-bold">teste</Text>
+            <SearchBox />
         </View>
     );
 };
