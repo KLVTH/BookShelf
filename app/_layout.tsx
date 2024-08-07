7198bdc3beecc3bed944f4b00d685224ab26f3e1
Kauã Oliveira Seixas <Kos281@hotmail.com>
2024-08-06 22:56:56 -0300

    develop: Componentizando as DrawerScreenOptions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,81 +1,60 @@
-import '../global.css'
+import React from 'react';
+import '../global.css';
 import { Ionicons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import CustomerDrawerContent from "~/components/CustomDrawerContent";
-import { HeaderButton } from '~/components/HeaderButton';
 import { useFonts, PlusJakartaSans_700Bold } from '@expo-google-fonts/plus-jakarta-sans';
+import CustomDrawerScreenOptions from '~/components/CustomDrawerScreenOptions';
 
 const DrawerLayout = () => {
-  
-  //! Essa não é a melhor maneira de implementar as fontes, Revise o código
+    
+  //Não é a forma ideal de implantar uma fonte, necessário revisão
   const [fontsLoaded] = useFonts({
-    PlusJakartaSans_700Bold,
+      PlusJakartaSans_700Bold,
   });
   if (!fontsLoaded) {
-    return null;
+      return null;
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        drawerContent={CustomerDrawerContent}
-        //Componentizar o screenOptions assim como fez com o drawerContent
-        screenOptions={({ navigation }) => ({
-          drawerActiveBackgroundColor: '#F24E1E',
-          drawerActiveTintColor: '#fff',
-          drawerLabelStyle: { marginLeft: -20 },
-          headerTitleStyle: {
-            fontFamily: 'PlusJakartaSans_700Bold', 
-            fontSize: 30,
-            marginLeft: -5
-          },
-          headerLeft: () => (
-            <Ionicons
-              name="menu"
-              size={40} 
-              color="black" 
-              style={{ marginLeft: 15, marginVertical: 12 }}
-              onPress={() => navigation.toggleDrawer()} // Abre/fecha o Drawer
-            />
-          ),
-          headerRight: () => (
-            <HeaderButton
-              onPress={() => {
-                console.log('Header button pressed!');
-              }}
-            />
-          ),
-        })}
-      >
-        <Drawer.Screen
-          name='index'
-          options={{
-            drawerLabel: 'Minha Estante',
-            headerTitle: 'Minha Estante',
-            drawerIcon: ({ size, color }) => (
-              <Ionicons name="library" size={size} color={color} />
-            ),
-            headerRight: () => (
-              <HeaderButton
-                onPress={() => {
-                  console.log('Header button pressed!');
-                }}
+        <Drawer
+          drawerContent={CustomerDrawerContent}
+          screenOptions={CustomDrawerScreenOptions}
+          >
+            <Drawer.Screen
+                  name='index'
+                  options={{
+                      drawerLabel: 'Minha Estante',
+                      headerTitle: 'Minha Estante',
+                      drawerIcon: ({ size, color }) => (
+                          <Ionicons name="library" size={size} color={color} />
+                      ),
+
+                  }}
               />
-            ),
-          }}
+              <Drawer.Screen
+                  name='profile'
+                  options={{
+                    drawerLabel: 'Meu Perfil',
+                    headerTitle: 'Meu Perfil',
+                    drawerIcon: ({ size, color }) => (
+                      <Ionicons name="person" size={size} color={color} />
+                    ),
+                  }}
         />
         <Drawer.Screen
-          name='profile'
-          options={{
-            drawerLabel: 'Meu Perfil',
-            headerTitle: 'Meu Perfil',
-            drawerIcon: ({ size, color }) => (
-              <Ionicons name="person" size={size} color={color} />
-            ),
-          }}
-        />
-      </Drawer>
+                  name='config'
+                  options={{
+                    drawerLabel: 'Configurações',
+                    headerTitle: 'Configurações',
+                    drawerIcon: ({ size, color }) => (
+                      <Ionicons name="settings" size={size} color={color} />
+                    ),
+                  }}
+              />
+        </Drawer>
     </GestureHandlerRootView>
   );
 };
