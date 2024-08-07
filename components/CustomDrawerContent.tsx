0acb2953c4749ebb5db051a62a57f61239e5fd1b
Kauã Oliveira Seixas <Kos281@hotmail.com>
2024-08-06 22:56:56 -0300

    feat: Componentizando as DrawerScreenOptions

--- components/CustomDrawerContent.tsx
+++ components/CustomDrawerContent.tsx
@@ -1,11 +1,17 @@
+//Adiciona uma imagem de perfil com nome
+
+import { Ionicons } from "@expo/vector-icons";
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
 import { useRouter } from 'expo-router';
 import { View, Text, Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function CustomerDrawerContent(props: any) {
+export default function CustomDrawerContent(props: any) {
     const router = useRouter()
-    const { top, bottom } = useSafeAreaInsets()
+    const { top, bottom } = useSafeAreaInsets() //Obtém os valores das margens seguras no topo e na parte inferior da tela.
+
+    //DrawerItemList - Renderiza a lista de itens do drawer.    
+    //DrawerItem - Adiciona o item logout, que ao ser pressionado redireciona o usuário para a página inicial usando router.replace('/')
 
     return (
         <View style={{ flex:1}}>
@@ -23,14 +29,22 @@ export default function CustomerDrawerContent(props: any) {
                         Kauã O. Seixas
                     </Text>
                 </View>
-
-                <View className="bg-white pt-2.5">
+            
+                <View className="bg-white pt-2.5"> 
                     <DrawerItemList {...props} />
-                    <DrawerItem label={"Logout"} onPress={() => router.replace('/')} />
+                    <DrawerItem
+                        label={"Logout"}
+                        onPress={() => router.replace('/')}
+                        icon={({size, color}) => (
+                            <Ionicons name="log-out" size={size} color={color} />
+                        )}
+                        
+                    />
                 </View>
+
             </DrawerContentScrollView>
             <View className="border-t-black border-t p-5 pb-${bottom+5}">
-                <Text>Bottom</Text>
+                <Text>Rodapé</Text>
             </View>
         </View>
     )
