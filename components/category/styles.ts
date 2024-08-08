aae5f112b74cb37d607631ff135a3b0fcefdbeb9
Kauã Oliveira Seixas <Kos281@hotmail.com>
2024-08-07 21:50:39 -0300

    develop: utilizando SectionList para categorização

---
+++ components/category/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 42,
+    paddingHorizontal: 16,
+    borderRadius: 7,
+    justifyContent: "center",
+    
+  },
+  title: {
+    color: "#F24E1E",
+    fontSize: 16,
+    fontWeight: "bold",
+    
+  },
+  selected: {
+    backgroundColor: "black",
+  },
+})
--- styles/colors.ts
+++ styles/colors.ts
@@ -3,6 +3,7 @@ export const colors = {
         200: "#828282",
         300: "#737373"
     },
+    'custom-light-gray': '#DBE5DE',
     
     orange: {
         400: "#DA7F64",
