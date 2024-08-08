e7d9d5375a62de406cd5e348cf3a8244cf81e090
Kauã Oliveira Seixas <Kos281@hotmail.com>
2024-08-07 21:50:39 -0300

    develop: utilizando SectionList para categorização

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,71 @@
-import { View, Text } from 'react-native';
-import SearchBox from '../components/SearchBox'; 
+import React, { useRef, useState } from "react"
+import { FlatList, SectionList, Text, View } from "react-native"
+
+import { styles } from "./styles"
+import { CATEGORIES, BOOKS } from "../utils/data"
+
+import { Book } from "../components/book"
+import { Category } from "../components/category"
 
 const Home = () => {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="bg-orange-500 font-bold">teste</Text>
-            <SearchBox />
-        </View>
-    );
-};
+  const [category, setCategory] = useState(CATEGORIES[0])
+  const sectionListRef = useRef<SectionList>(null)
 
-export default Home;
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    )
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      })
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View className="border-b-2 border-custom-light-gray ">
+        <FlatList
+          data={CATEGORIES}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Category
+              title={item}
+              onPress={() => handleCategorySelect(item)}
+              isSelected={item === category}
+            />
+          )}
+          style={styles.categories}
+          contentContainerStyle={{
+            gap: 12,
+            paddingHorizontal: 32,
+          }}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+        />
+      </View>
+
+      <SectionList style={{width: '100%'}}
+        ref={sectionListRef}
+        sections={BOOKS}
+        keyExtractor={(item) => item}
+        stickySectionHeadersEnabled={false}
+        renderItem={() => <Book />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.sectionContainer}
+        //Talvez seja possível ao implementar a busca por voz a procura pela sessão
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </View>
+  )
+}
+
+export default Home
+
---
+++ utils/data.ts
@@ -0,0 +1,26 @@
+const BOOKS = [
+    {
+      title: "Favoritos",
+      data: Array.from({ length: 5 }, (_, index) => String(index)),
+    },
+    {
+      title: "Recentes",
+      data: Array.from({ length: 2 }, (_, index) => String(index)),
+    },
+    {
+      title: "Livros",
+      data: Array.from({ length: 10 }, (_, index) => String(index)),
+    },
+    {
+      title: "Documentos",
+      data: Array.from({ length: 7 }, (_, index) => String(index)),
+    },
+    {
+      title: "AudioBook",
+      data: Array.from({ length: 2 }, (_, index) => String(index)),
+    },
+  ]
+  
+  const CATEGORIES = BOOKS.map((item) => item.title)
+  
+  export { BOOKS, CATEGORIES }
