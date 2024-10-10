4f49496b5904f2a2cada610c0123e9e9dc047df1
Kauã Oliveira Seixas <Kos281@hotmail.com>
2024-10-10 00:08:05 -0300

    feat(pdfviewer): Criar botão que leve a tela de informação do pdf

--- src/app/(home)/pdfviewer.tsx
+++ src/app/(home)/pdfviewer.tsx
@@ -1,12 +1,21 @@
-import { useLocalSearchParams } from "expo-router";
-import { StyleSheet, Dimensions, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { StyleSheet, Dimensions, View, Button } from "react-native";
 import Pdf from "react-native-pdf";
 import { useEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native'; // Importe o hook de navegação
+
 
 const pdfpage = () => {
   const { name, uri } = useLocalSearchParams<{ name: string; uri: string }>();
-  const navigation = useNavigation(); 
+  const navigation = useNavigation(); // Obtém o objeto de navegação
+
+  const openPDFInfo = () => {
+    router.push({
+      pathname: "/pdfInfo",
+      params: { name, uri },
+    });
+  };
+
   useEffect(() => {
     // Define o título do cabeçalho com o nome do PDF
     navigation.setOptions({ title: name });
@@ -14,6 +23,7 @@ const pdfpage = () => {
 
   return (
     <View style={styles.container}>
+      <Button title="Info" onPress={openPDFInfo}></Button>
       <Pdf
         source={{
           uri,
